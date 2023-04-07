4e8611673279eb04c0787056a31913b63aaf6e11
ColbyNeves1999 <120044465+ColbyNeves1999@users.noreply.github.com>
2023-04-06 19:09:49 -0500

    Revert "comment"

--- src/models/SongModel.ts
+++ src/models/SongModel.ts
@@ -5,19 +5,19 @@ const songRepository = AppDataSource.getRepository(Song);
 
 async function addSong(songTitle: string, songId: string, artist: string): Promise<Song> {
 
-  // Create the new user object
-  let newSong = new Song();
-  newSong.songID = songId;
-  newSong.songTitle = songTitle;
-  newSong.artist = artist;
+    // Create the new user object
+    let newSong = new Song();
+    newSong.songID = songId;
+    newSong.songTitle = songTitle;
+    newSong.artist = artist;
+  
+    // Then save it to the database
+    // NOTES: We reassign to `newUser` so we can access
+    // NOTES: the fields the database autogenerates (the id & default columns)
+    newSong = await songRepository.save(newSong);
+  
+    return newSong;
+  
+  }
 
-  // Then save it to the database
-  // NOTES: We reassign to `newUser` so we can access
-  // NOTES: the fields the database autogenerates (the id & default columns)
-  newSong = await songRepository.save(newSong);
-
-  return newSong;
-
-}
-// I AM MAKING A TEST CHANGE
-export { addSong };
+  export {addSong};
